4fefd7e74951cc5e4aa136b85987742ec0061de0
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-02-11 00:11:11 -0800

    More setup code
    Code to upload a Gif. not complete.

---
+++ app/src/main/java/com/example/gifiks/GifManager.java
@@ -0,0 +1,12 @@
+package com.example.gifiks;
+
+import android.widget.ImageView;
+
+public class GifManager {
+    com.google.android.material.floatingactionbutton.FloatingActionButton AddGif;
+    ImageView ViewGif;
+    public void Upload(){
+
+    }
+
+}
