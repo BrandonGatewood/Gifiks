dea8037db95c1bc70fc7873c04ac8ab4a70cea72
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-02-23 06:43:06 -0800

    set up the gallery fragment

---
+++ app/src/main/java/com/example/gifiks/Gallery.java
@@ -0,0 +1,44 @@
+package com.example.gifiks;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.gifiks.databinding.FragmentGalleryBinding;
+
+public class Gallery extends Fragment {
+    private FragmentGalleryBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
