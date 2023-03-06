937e61e9c0c3a71a8af3c103c8869c5054499fa6
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-03-06 05:56:42 -0800

    working on uploading a gif to a directory

--- app/src/main/java/com/example/gifiks/UploadGif.java
+++ app/src/main/java/com/example/gifiks/UploadGif.java
@@ -2,6 +2,7 @@ package com.example.gifiks;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,12 +14,29 @@ import android.widget.ImageView;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.gifiks.databinding.UploadGifBinding;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.channels.FileChannel;
 import java.util.Objects;
 
 //to upload a gif it must be in the memory of the phone. one place to add them is at data/media/0/Pictures
@@ -27,6 +45,11 @@ public class UploadGif extends Fragment {
     ImageView ViewGif;
 
     private UploadGifBinding binding;
+
+    String user;
+    File directory;
+    File gifdirectory;
+    String gifName;
 
     @Override
     public View onCreateView(
@@ -43,6 +66,11 @@ public class UploadGif extends Fragment {
 
         Bundle bundle = this.getArguments();
         Account receivedAccount = Objects.requireNonNull(bundle).getParcelable("AccountInfo");
+        user = "Gifs/" + receivedAccount.getUsername();
+
+        directory = Objects.requireNonNull(this.getContext()).getDataDir();
+
+        gifdirectory = new File(directory, user);
 
         binding.AddGif.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,11 +103,81 @@ public class UploadGif extends Fragment {
                     Intent data = result.getData();
                     if (data != null
                             && data.getData() != null) {
-                        Uri selectedImageUri = data.getData();
-                        Glide.with(this).load(selectedImageUri).into(ViewGif);
+                        Uri uri = data.getData();
+                        String selectedImageUri = data.getData().toString();
+                        gifName = selectedImageUri.substring(selectedImageUri.lastIndexOf("%2F") + 3);
+                        Glide.with(this).asGif().load(selectedImageUri).into(ViewGif);
+                        saveGif(uri);
+                        /*Target<File> fileTarget = Glide.with(this).asFile()
+                                .load(selectedImageUri)
+                                .listener(new RequestListener<File>() {
+                                    @Override
+                                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<File> target, boolean isFirstResource) {
+                                        return false;
+                                    }
+
+                                    @Override
+                                    public boolean onResourceReady(File resource, Object model, Target<File> target, DataSource dataSource, boolean isFirstResource) {
+                                        return false;
+                                    }
+                                })
+                                .into(new Target<File>() {
+                                    @Override
+                                    public void onStart() {}
+                                    @Override
+                                    public void onStop() {}
+                                    @Override
+                                    public void onDestroy() {}
+                                    @Override
+                                    public void onLoadStarted(@Nullable Drawable placeholder) {}
+                                    @Override
+                                    public void onLoadFailed(@Nullable Drawable errorDrawable) {}
+                                    @Override
+                                    public void onResourceReady(@NonNull File resource, @Nullable Transition<? super File> transition) {
+                                        saveGif(resource);
+                                    }
+                                    @Override
+                                    public void onLoadCleared(@Nullable Drawable placeholder) {}
+                                    @Override
+                                    public void getSize(@NonNull SizeReadyCallback cb) {}
+                                    @Override
+                                    public void removeCallback(@NonNull SizeReadyCallback cb) {}
+                                    @Override
+                                    public void setRequest(@Nullable Request request) {}
+                                    @Nullable
+                                    @Override
+                                    public Request getRequest() {
+                                        return null;
+                                    }
+                                });*/
+                        // File gifFile = new File(fileTarget);
+                        //Glide.with(this).asGif().load(selectedImageUri).into(fileTarget);
                     }
                 }
             });
+
+    private void saveGif(Uri uri) {
+        File newGifFile = new File(gifdirectory, gifName);
+
+        //String imageFilePath = FilenameUtils.getPath(uri);
+        File selectedGifFile = new File("/data/media/0/Pictures/" + gifName);
+        if (newGifFile == null) {
+            return;
+        }
+        try {
+            FileOutputStream output = new FileOutputStream(newGifFile);
+            FileInputStream input = new FileInputStream(selectedGifFile);
+
+            FileChannel inputChannel = input.getChannel();
+            FileChannel outputChannel = output.getChannel();
+
+            inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+            output.close();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onDestroyView() {
