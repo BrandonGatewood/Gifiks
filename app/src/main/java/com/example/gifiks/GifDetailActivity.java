e289cad1563ba9f1991b3073a435f2f2daf58f37
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-03-07 00:44:00 -0800

    Upload gif working

--- app/src/main/java/com/example/gifiks/GifDetailActivity.java
+++ app/src/main/java/com/example/gifiks/GifDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,6 +20,7 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -33,18 +35,16 @@ public class GifDetailActivity extends AppCompatActivity {
     private ImageView imageView;
     private float mScaleFactor = 1.0f;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = GifDetailActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-
         imgPath = getIntent().getStringExtra("imgPath");
         imageView = findViewById(R.id.idIVImage);
 
         Glide.with(this).load(imgPath).placeholder(R.drawable.ic_launcher_background).into(imageView);
     }
-
 }
