dfc8e00b600e9f6f9845889dbef38c0d782b84aa
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-03-18 18:22:50 -0700

    Unit tests for UploadGif and Gallery classes
    A couple of unit test for UploadGif and Gallery classes.

--- app/src/main/java/com/example/gifiks/GifDetailActivity.java
+++ app/src/main/java/com/example/gifiks/GifDetailActivity.java
@@ -2,29 +2,10 @@ package com.example.gifiks;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import android.content.Intent;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.example.gifiks.databinding.ActivityMainBinding;
 import com.example.gifiks.databinding.GifDetailActivityBinding;
 
 import java.io.File;
