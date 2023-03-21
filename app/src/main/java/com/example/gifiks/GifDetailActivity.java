fd9bb085962d1a583512cf46ff14cf175e251801
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-03-21 14:11:28 -0700

    comment bug fixed
    keyboard will not overlay the page and not try to resize it

--- app/src/main/java/com/example/gifiks/GifDetailActivity.java
+++ app/src/main/java/com/example/gifiks/GifDetailActivity.java
@@ -1,17 +1,13 @@
 package com.example.gifiks;
 
-import android.os.Build;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.fragment.NavHostFragment;
-
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.gifiks.databinding.GifDetailActivityBinding;
@@ -20,7 +16,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +31,7 @@ public class GifDetailActivity extends AppCompatActivity {
     private TextView commentView;
     private TextView userNameView;
     private TextView gifNameView;
+    private EditText viewYourComment;
 
     private String userName;
     private String gifName;
@@ -55,8 +51,7 @@ public class GifDetailActivity extends AppCompatActivity {
         userNameView = findViewById(R.id.ByAUser);
         gifNameView = findViewById(R.id.AGifName);
         comment = findViewById(R.id.comment);
-
-        final EditText viewYourComment = findViewById(R.id.yourComment);
+        viewYourComment = findViewById(R.id.yourComment);
 
         getComments();
 
